9261eb9c8b2931e2eb382f347d954225707b619a
Florian Rappl <florian.rappl@smapiot.com>
2020-01-18 14:52:47 +0100

    Upgraded packages

--- elm/src/index.ts
+++ elm/src/index.ts
@@ -5,26 +5,8 @@ import { Elm } from './Tile.elm';
  * Shows an API extension using an Elm module.
  */
 export function setup(piral: PiletApi) {
-  // piral.registerTile(piral.fromElm(Elm.Tile), {
-  //   initialColumns: 2,
-  //   initialRows: 2,
-  // });
-
-  piral.registerTile(
-    {
-      type: 'html',
-      component: {
-        mount(node, props) {
-          Elm.Tile.init({
-            node,
-            flags: props,
-          });
-        },
-      },
-    },
-    {
-      initialColumns: 2,
-      initialRows: 2,
-    },
-  );
+  piral.registerTile(piral.fromElm(Elm.Tile), {
+    initialColumns: 2,
+    initialRows: 2,
+  });
 }
