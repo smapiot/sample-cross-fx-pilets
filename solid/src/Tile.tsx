0125bbabad27f1451234e743e6474b4ebe7167dd
Florian Rappl <mail@florian-rappl.de>
2023-04-11 10:49:54 +0200

    Updated to use v1 of Solid

--- solid/src/Tile.tsx
+++ solid/src/Tile.tsx
@@ -1,18 +1,18 @@
-import { createState, Component } from 'solid-js';
+import { createSignal, Component } from 'solid-js';
 import { TileComponentProps } from 'sample-cross-fx';
 
 export const Tile: Component<TileComponentProps> = ({ piral, rows, columns }) => {
-  const [state, setState] = createState({ count: 0 });
+  const [getCount, setCount] = createSignal(0);
   const { SolidExtension } = piral;
 
   return (
     <div class="tile">
-      <h3>Solid {state.count}</h3>
+      <h3>Solid {getCount()}</h3>
       <p>
         {rows} rows and {columns} columns <SolidExtension name="smiley" />
       </p>
-      <button onClick={() => setState({ count: state.count + 1 })}>+</button>
-      <button onClick={() => setState({ count: state.count - 1 })}>-</button>
+      <button onClick={() => setCount(getCount() + 1)}>+</button>
+      <button onClick={() => setCount(getCount() - 1)}>-</button>
     </div>
   );
 };
